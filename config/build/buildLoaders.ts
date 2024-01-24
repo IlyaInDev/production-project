5a7df322ccac29a326cf86b085421eece426ca49
Ilya <emptydesert29@gmail.com>
2024-01-24 11:39:20 +0300

    add babel add extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -28,6 +28,26 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 		],
 	};
 
+	const babelLoader = {
+		test: /\.(js|jsx|tsx)$/,
+		exclude: /node_modules/,
+		use: {
+			loader: "babel-loader",
+			options: {
+				presets: ['@babel/preset-env'],
+				plugins: [
+					[
+						"i18next-extract",
+						{
+							locales: ['ru', 'en'],
+							keyAsDefaultValue: true
+						}
+					],
+				]
+			},
+		}
+	}
+
 	const tsLoader = {
 		test: /\.tsx?$/,
 		use: 'ts-loader',
@@ -46,7 +66,8 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 	return [
 		fileLoader,
 		svgLoader,
-		cssLoader,
-		tsLoader
+		babelLoader,
+		tsLoader,
+		cssLoader
 	]
 }
