bf286670441da1fcfed2d6772cb5a566800dfe56
Ilya <emptydesert29@gmail.com>
2024-01-31 13:55:48 +0300

    add react testing library, add tests for btn and sidebar

---
+++ src/shared/lib/tests/renderWithTranslations/renderWithTranslations.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from 'react';
+import { I18nextProvider } from 'react-i18next';
+import { render } from '@testing-library/react';
+import i18nForTesting from '../../../config/i18n/i18nForTesting';
+
+export function renderWithTranslation(component: ReactNode) {
+    return render(
+        <I18nextProvider i18n={i18nForTesting}>
+            {component}
+        </I18nextProvider>,
+    );
+}
