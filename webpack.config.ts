6dd5932ae22f35382b6a9e8d7c15a8b4c98d849f
Ilya <emptydesert29@gmail.com>
2024-03-28 14:48:42 +0300

    updated path to index.html file in webpack.config

--- webpack.config.ts
+++ webpack.config.ts
@@ -6,7 +6,7 @@ import { BuildEnv, BuildPaths } from './config/build/types/config';
 export default (env: BuildEnv) => {
     const paths: BuildPaths = {
         entry: path.resolve(__dirname, 'src', 'index.tsx'),
-        html: path.resolve(__dirname, 'public', 'index.html'),
+        html: path.resolve(__dirname, 'index.html'),
         build: path.resolve(__dirname, 'build'),
         src: path.resolve(__dirname, 'src'),
         locales: path.resolve(__dirname, 'public', 'locales'),
